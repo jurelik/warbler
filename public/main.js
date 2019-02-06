430b76c4fe52772aaf77bd208d3412ce26f6741f
jurelik <jure@likar.me>
2019-02-06 21:10:53 +0000

    basic styling and audio context init

--- public/main.js
+++ public/main.js
@@ -1,1 +1,15 @@
-console.log('sup');
+const AudioContext = window.AudioContext || window.webkitAudioContext;
+const context = new AudioContext();
+
+class Tape {
+  constructor() {
+    this.buffer;
+  }
+
+  test() {
+    this.buffer = 'sup';
+    console.log(this.buffer);
+  }
+}
+
+let tape = new Tape();
