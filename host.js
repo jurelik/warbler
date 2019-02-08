66c2f4c9c23f3eea9b2fa77f664322d0054e09b1
jurelik <jure@likar.me>
2019-02-08 19:53:32 +0000

    loading of preloaded mp3 file works

--- host.js
+++ host.js
@@ -1,8 +1,17 @@
+const fs = require('fs');
 const express = require('express');
+const send = require('send');
+const ytmp3 = require('youtube-mp3-downloader');
+
 const server = express();
 
 server.use(express.static('./public'));
 
+server.get('/download/', (req, res) => {
+  console.log('req made');
+  send(req, 'public/downloads/comp 250.mp3').pipe(res);
+})
+
 server.listen(3000, () => {
-  console.log('listening on port 3000');
-});
+  console.log('listening to 3000');
+})
--- public/main.js
+++ public/main.js
@@ -4,11 +4,54 @@ const context = new AudioContext();
 class Tape {
   constructor() {
     this.buffer;
+    this.revBuffer;
   }
 
-  test() {
-    this.buffer = 'sup';
-    console.log(this.buffer);
+  load() {
+    fetch(`http://localhost:3000/download/`)
+    .then(res => {
+      return res.arrayBuffer();
+    })
+    .then(res => {
+      context.decodeAudioData(res, decoded => {
+        this.buffer = decoded;
+        console.log('audio decoded');
+        console.log(this.buffer);
+      });
+    })
+    .catch(err => {
+      console.log(err);
+    });
+
+
+
+
+
+    // fetch(`http://localhost:3000/download/${id}`)
+    // .then(res => {
+    //   return res.arrayBuffer();
+    // })
+    // .then(res => {
+    //   context.decodeAudioData(res, decoded => {
+    //     this.buffer = decoded;
+    //     console.log('audio decoded');
+    //     console.log(this.buffer);
+    //   });
+    // })
+    // .catch(err => {
+    //   console.log(err);
+    // });
+
+    // let request = new XMLHttpRequest();
+    // request.open('GET', `http://localhost:3000/download/${id}`, true);
+    // // request.responseType = 'arraybuffer';
+    // request.onload = function() {
+    //   console.log(request.response);
+    //   // context.decodeAudioData(request.response, decoded => {
+    //   //   console.log('audio decoded');
+    //   // });
+    // };
+    // request.send();
   }
 }
 
