253810600abef00e4a7495b3aa79057cc67c430f
jurelik <jure@likar.me>
2019-02-23 19:17:16 +0000

    all buttons working in playing and not playing state

--- public/tape.js
+++ public/tape.js
@@ -16,51 +16,106 @@ class Tape {
     this.playBtn = document.getElementById('play-pause');
     this.playBtnIcon = document.getElementById('play-pause-icon');
     this.playBtn.addEventListener('click', () => {
-      if(!this.playing) {
+      if(!this.playing && this.buffer != null) { //If song is not playing
         this.play(this.buffer);
         this.playing = true;
         this.playBtnIcon.className = "fas fa-pause";
       }
-      else {
+      else if (this.playing && this.buffer != null){ //If song is playing
         this.pause();
         this.playing = false;
         this.playBtnIcon.className = "fas fa-play";
+      }
+      else { //If no song is loaded
+        console.error('Error: No sound loaded');
       }
     });
 
     //Forward Button
     this.forwardBtn = document.getElementById('forward');
     this.forwardBtn.addEventListener('mousedown', () => {
-      if (this.playing) {
+      if (this.playing && this.buffer != null) { //If song is playing
         this.updatePosition(); //Updates the position until this moment
         this.source.playbackRate.value = 1.5;
         this.mouseDownTime = context.currentTime;
       }
+      else if (!this.playing && this.buffer != null) { //If song is not playing
+        this.play(this.buffer);
+        this.source.playbackRate.value = 1.5;
+        this.mouseDownTime = context.currentTime;
+      }
+      else { //If no song is loaded
+        console.error('Error: No sound loaded');
+      }
     });
     this.forwardBtn.addEventListener('mouseup', () => {
-      if (this.playing) {
+      if (this.playing && this.buffer != null) { //If song is playing
         this.startTime = context.currentTime;
         this.source.playbackRate.value = 1;
         this.currentPosition += (context.currentTime - this.mouseDownTime) * 1.5; //Updates the position after the fast forward
+      }
+      else if (!this.playing && this.buffer != null) { //If song is not playing
+        this.source.stop();
+        this.currentPosition += (context.currentTime - this.mouseDownTime) * 1.5;
+      }
+      else { //If no song is loaded
+        console.error('Error: No sound loaded');
       }
     });
 
     //Rewind Button
     this.rewindBtn = document.getElementById('rewind');
     this.rewindBtn.addEventListener('mousedown', () => {
-      if (this.playing) {
+      if (this.playing && this.buffer != null) { //If song is playing
         this.source.stop();
         this.updatePosition(); //Updates the position until this moment
         this.play(this.revBuffer);
         this.source.playbackRate.value = 1.5;
         this.mouseDownTime = context.currentTime;
       }
+      else if (!this.playing && this.buffer != null) { //If song is not playing
+        this.play(this.revBuffer);
+        this.source.playbackRate.value = 1.5;
+        this.mouseDownTime = context.currentTime;
+      }
+      else { //If no song is loaded
+        console.error('Error: No sound loaded')
+      }
     });
     this.rewindBtn.addEventListener('mouseup', () => {
-      if(this.playing) {
+      if(this.playing && this.buffer != null) { //If song is playing
         this.source.stop();
         this.currentPosition -= (context.currentTime - this.mouseDownTime) * 1.5; //Updates the position until this moment
+        if (this.currentPosition < 0) { //Make sure currentPosition doesn't go below 0
+          this.currentPosition = 0;
+        }
         this.play(this.buffer);
+      }
+      else if (!this.playing && this.buffer != null) { //If song is not playing
+        this.source.stop();
+        this.currentPosition -= (context.currentTime - this.mouseDownTime) * 1.5;
+        if (this.currentPosition < 0) {
+          this.currentPosition = 0;
+        }
+      }
+      else { //If no song is loaded
+        console.error('Error: No sound loaded');
+      }
+    });
+
+    //Rewind-Full Button
+    this.rewindFullBtn = document.getElementById('rewind-full');
+    this.rewindFullBtn.addEventListener('click', () => {
+      if (this.playing && this.buffer != null) { //If song is playing
+        this.source.stop();
+        this.currentPosition = 0;
+        this.play(this.buffer);
+      }
+      else if (!this.playing && this.buffer != null){ //If song is not playing
+        this.currentPosition = 0;
+      }
+      else { //If no song is loaded
+        console.error('Error: No sound loaded')
       }
     });
   }
@@ -105,7 +160,7 @@ class Tape {
       this.source.start(context.currentTime, this.duration - this.currentPosition);
     }
     else {
-      console.log('Error: No buffer found in the play method');
+      console.error('Error: No buffer found in the play method');
     }
   }
 
