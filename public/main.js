ed5eed3b9c6d6a1e591604cf5c357638bff75229
jurelik <jure@likar.me>
2019-03-29 22:22:47 +0000

    testing out regex for youtube video ID

--- public/main.js
+++ public/main.js
@@ -2,3 +2,8 @@ const AudioContext = window.AudioContext || window.webkitAudioContext;
 const context = new AudioContext();
 
 let tape = new Tape();
+
+function testRegex(id) {
+  let split = id.split('v=');
+  console.log(split[1]);
+}
